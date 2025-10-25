78fa2321c5e06ff6387731045a6a96c9b0b6152b
ozzy8160 <ryanpctech81@gmail.com>
2025-10-24 20:00:39 -0400

    cleaning up nix config and gpu stuff

--- configuration.nix
+++ configuration.nix
@@ -11,15 +11,31 @@
       ./terminal/fonts.nix
 #      ./terminal/starship.nix
     ];
-
+  #power managament
+  powerManagement.enable = true;
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi = { 
+      canTouchEfiVariables = true;
+    };
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 5;
+    };
+  };
 
-  #garbage collection
-  nix.settings.auto-optimise-store = true;
-  nix.gc.automatic = true;
-  nix.gc.options = "--delete-older-than +5"; 
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      # enable flakes
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+    #garbage collection
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 10d";
+    };
+  };
 
   #kernel version
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -27,13 +43,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   
-  # enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # enable bluetooth
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = false; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
 
   # Enable networking and set host name
   networking.networkmanager.enable = true;
@@ -57,42 +69,43 @@
   };
   
   #hyprland
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    withUWSM = true;
- };
-
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  programs ={
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+      withUWSM = true;
+    };
+    starship = {
+      enable = true;
+      # Configuration written to ~/.config/starship.toml
+      settings = {
+        add_newline = false;
+      };
+    };
+    fuse = {
+      userAllowOther = true;
+    };
   };
+
+
+  #  nixpkgs.config.packageOverrides = pkgs: {
+  #    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  #  };
   # enable opengl
   hardware.graphics = {
       # Opengl
       enable = true;
       extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      libvdpau-va-gl
-      vpl-gpu-rt
+      libva-vdpau-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      intel-compute-runtime-legacy1
+      #intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      #libvdpau-va-gl
+      #vpl-gpu-rt
     ];
   };
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+#  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
   
-  # startship
-  programs.starship = {
-    enable = true;
-   # Configuration written to ~/.config/starship.toml
-    settings = {
-      add_newline = false;
-    };
-  };
-
-  services.getty.autologinUser = "ryan";
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
 #  # Enable sound with pipewire.
 #  sound.enable = true;
 #  hardware.pulseaudio.enable = false;
@@ -110,14 +123,14 @@
     #media-session.enable = true;
 #  };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
+  virtualisation.libvirtd.enable = true;
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ryan = {
     isNormalUser = true;
     description = "ryan";
-    extraGroups = [ "networkmanager" "wheel" "dialout" ];
+    extraGroups = [ "networkmanager" "wheel" "dialout" "qemu-libvirtd" "libvirtd"];
     packages = with pkgs; [
       floorp-bin
     ];
@@ -126,19 +139,17 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    arduino
-    arduino-ide
+    neovim
+    bash-completion
+    blueman
     brightnessctl
     btop
-    cmatrix
     dunst
     fastfetch
     fd
     ffmpeg
     fzf
     gcc
-    gparted
     git
     grim
     hyprcursor
@@ -150,13 +161,14 @@
     kitty
     lazygit
     lsd
-    waybar
     libnotify
+    libvirt
+    networkmanagerapplet
+    pywal
+    qemu
     rclone
     rofi
     ripgrep
-    networkmanagerapplet
-    onlyoffice-desktopeditors
     sftpman
     sshfs
     slurp
@@ -169,15 +181,18 @@
     pulseaudio
     pipewire
     wf-recorder
+    waybar
     wget
     tldr
     timeshift
+    virt-manager
     xclip
     zip
     zoxide
   ];
-
+  
   # bash stuff here
+  environment.pathsToLink = [ "/share/bash-completion" ];
   programs = {
     command-not-found.enable = false;
 
@@ -187,19 +202,17 @@
       #set bash aliases here
       shellAliases = {
         b = "cd ..";
-#	ls = "lsd";
-	sj = "ssh ryan@192.168.1.58";
-	sn = "ssh ryan@192.168.1.114";
-	# Search command line history
-	h = "history | grep ";
-	#vim
-	v = "nvim";
-	sv = "sudo nvim";
-	#nixos
-	rebuild = "sudo nixos-rebuild switch --flake $(readlink -f /home/ryan/.dotfiles/flakes)";
-	updt = "cd ~/.dotfiles/flakes/ && sudo nix flake update";
-	#git
-	add = "git add ."; 
+      #	ls = "lsd";
+      	# Search command line history
+      	h = "history | rp ";
+        	#vim
+        	v = "nvim";
+        	sv = "sudo nvim";
+        	#nixos
+        	rebuild = "sudo nixos-rebuild switch --flake $(readlink -f /home/ryan/.dotfiles/flakes)";
+        	updt = "cd ~/.dotfiles/flakes/ && sudo nix flake update";
+        	#git
+        	add = "git add ."; 
 	
       };
     };
@@ -213,7 +226,26 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
+  #all services enable:
+  services = {
+    blueman = {
+      enable = true;
+    };
+    getty = {
+      autologinUser = "ryan";
+    };
+    printing = {
+      enable = true;
+    };
+    qemuGuest = {
+      enable = true;
+    };
+    spice-vdagentd = {
+      enable = true;
+    };
+  };
+
+
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
