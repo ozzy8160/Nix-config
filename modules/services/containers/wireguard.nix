8412bdc70f76812a3097c0497e1ee07a720cc8d5
ozzy8160 <ryanpctech81@gmail.com>
2026-02-04 21:00:14 -0500

    fix wireguard.nix syntax

--- modules/services/containers/wireguard.nix
+++ modules/services/containers/wireguard.nix
@@ -1,15 +1,12 @@
-{ pkgs, ... }: 
 {
   virtualisation = {
-    podman = {
-      enable = true;
       # Required for containers in a pod to communicate (e.g., via podman-compose)
       defaultNetwork.settings.dns_enabled = true;
     };
     oci-containers = {
       backend = "podman";
     };
-    containers."wg-easy" = {
+    containers.containers."wireguard" = {
       image = "ghcr.io/wg-easy/wg-easy:latest";
       ports = [
         "51820:51820/udp"
@@ -27,7 +24,6 @@
         "--sysctl=net.ipv4.conf.all.src_valid_mark=1"
       ];
     };
-  };
   # Enable IP forwarding for VPN routing
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
@@ -35,6 +31,24 @@
   };
 }
 
+#virtualisation.oci-containers.containers."wireguard" = {
+#      image = "ghcr.io/wg-easy/wg-easy:latest";
+#      ports = [
+#        "51820:51820/udp"
+#        "51821:51821/tcp"
+#      ];
+#      environment = {
+#        WG_HOST = " 47.231.219.6";
+#        PASSWORD = "test";
+#      };
+#      volumes = [
+#        "/var/lib/wg-easy:/etc/wireguard"
+#      ];
+#      extraOptions = [
+#        "--cap-add=NET_ADMIN"
+#        "--sysctl=net.ipv4.conf.all.src_valid_mark=1"
+#      ];
+#    };
 #{ pkgs, ... }: {
 #  # 1. Pre-load the WireGuard kernel module (Rootless cannot do this)
 #  boot.kernelModules = [ "wireguard" ];
